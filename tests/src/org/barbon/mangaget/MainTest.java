eeada4a90240ddf928d4d7d19ab2afbd6efcadfe
Mattia Barbon <mattia@barbon.org>
2011-08-14 12:22:17 +0200

    Added test application skeleton.

---
+++ tests/src/org/barbon/mangaget/MainTest.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) Mattia Barbon <mattia@barbon.org>
+ * distributed under the terms of the MIT license
+ */
+
+package org.barbon.mangaget;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class MainTest extends ActivityInstrumentationTestCase2<Main> {
+    public MainTest() {
+        super("org.barbon.mangaget", Main.class);
+    }
+}
