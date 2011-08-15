433f8d30b504c2f4218ad7208d8dbc613150a048
Mattia Barbon <mattia@barbon.org>
2011-08-15 18:53:50 +0200

    Added basic scraper implementation to get a chapter from AnimeA.net.

---
+++ src/org/barbon/mangaget/scrape/animea/Scraper.java
@@ -0,0 +1,354 @@
+/*
+ * Copyright (c) Mattia Barbon <mattia@barbon.org>
+ * distributed under the terms of the MIT license
+ */
+
+package org.barbon.mangaget.scrape.animea;
+
+import android.content.ContentValues;
+import android.content.Context;
+
+import android.database.Cursor;
+
+import java.io.File;
+import java.io.InputStream;
+import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.barbon.mangaget.CBZFile;
+
+import org.barbon.mangaget.data.DB;
+
+import org.barbon.mangaget.scrape.Downloader;
+
+import org.jsoup.Jsoup;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import org.jsoup.select.Elements;
+
+public class Scraper {
+    private DB db;
+    private Downloader downloader;
+
+    private class ChapterDownload {
+        public long id;
+        public ContentValues chapter;
+        public String targetPath, tempDir;
+        public OnChapterDownloadProgress listener;
+    }
+
+    private class PageDownload {
+        public long id;
+        public String url;
+        public String imageUrl;
+        public String targetPath;
+        public int status;
+    }
+
+    private class ChapterDownloader
+            extends Downloader.OnDownloadProgressAdapter {
+        private ChapterDownload download;
+        private Downloader.DownloadDestination target;
+
+        public ChapterDownloader(ChapterDownload info) {
+            super();
+
+            download = info;
+        }
+
+        public void start() {
+            target = downloader.requestDownload(
+                download.chapter.getAsString(DB.CHAPTER_URL), this);
+        }
+
+        @Override
+        public void downloadStarted() {
+            super.downloadStarted();
+
+            db.updateChapterStatus(download.id, DB.DOWNLOAD_STARTED);
+        }
+
+        @Override
+        public void downloadComplete(boolean success) {
+            super.downloadComplete(success);
+
+            if (!success) {
+                db.updateChapterStatus(download.id, DB.DOWNLOAD_REQUESTED);
+                download.listener.downloadComplete(success);
+
+                return;
+            }
+
+            List<String> pageUrls = scrapeChapterPages(target);
+
+            int index = 0;
+            for (String url : pageUrls)
+                db.insertPage(download.id, index++, url, null,
+                              DB.DOWNLOAD_REQUESTED);
+
+            db.updateChapterStatus(download.id, DB.DOWNLOAD_COMPLETE);
+            downloadPages(download);
+        }
+    }
+
+    private class PageDownloader {
+        private class PageInfoDownloader
+                extends Downloader.OnDownloadProgressAdapter {
+            private PageDownload page;
+            private Downloader.DownloadDestination target;
+
+            public PageInfoDownloader(PageDownload _page) {
+                page = _page;
+            }
+
+            public void start() {
+                target = downloader.requestDownload(page.url, this);
+            }
+
+            @Override
+            public void downloadStarted() {
+                super.downloadStarted();
+
+                db.updatePageStatus(page.id, DB.DOWNLOAD_STARTED);
+            }
+
+            @Override
+            public void downloadComplete(boolean success) {
+                super.downloadComplete(success);
+
+                if (!success) {
+                    db.updatePageStatus(page.id, DB.DOWNLOAD_REQUESTED);
+                    download.listener.downloadComplete(success);
+
+                    // TODO stop all downloads
+
+                    return;
+                }
+
+                page.imageUrl = scrapeImageUrl(target);
+                db.updatePageImage(page.id, page.imageUrl);
+
+                count -= 1;
+
+                downloadPageImage(page);
+            }
+        }
+
+        private class PageImageDownloader
+                extends Downloader.OnDownloadProgressAdapter {
+            private PageDownload page;
+
+            public PageImageDownloader(PageDownload _page) {
+                page = _page;
+            }
+
+            public void start() {
+                downloader.requestDownload(page.imageUrl, this,
+                                           new File(page.targetPath));
+            }
+
+            @Override
+            public void downloadStarted() {
+                super.downloadStarted();
+
+                db.updatePageStatus(page.id, DB.DOWNLOAD_STARTED);
+            }
+
+            @Override
+            public void downloadComplete(boolean success) {
+                super.downloadComplete(success);
+
+                if (!success) {
+                    db.updatePageStatus(page.id, DB.DOWNLOAD_REQUESTED);
+                    download.listener.downloadComplete(success);
+
+                    // TODO stop all downloads
+
+                    return;
+                }
+
+                db.updatePageStatus(page.id, DB.DOWNLOAD_COMPLETE);
+                count -= 1;
+
+                // TODO rename the image according to image type
+                //      using magic + content-type + (real) url
+
+                if (count == 0) {
+                    createChapterArchive(download, pages);
+                    download.listener.downloadComplete(true);
+                }
+            }
+        }
+
+        private ChapterDownload download;
+        private List<PageDownload> pages;
+        private int count;
+
+        public PageDownloader(ChapterDownload _download,
+                              List<PageDownload> _pages) {
+            download = _download;
+            pages = _pages;
+        }
+
+        public void start() {
+            for (PageDownload page : pages) {
+                if (page.imageUrl == null) {
+                    count += 2;
+
+                    downloadPageInfo(page);
+                }
+                else if (page.status != DB.DOWNLOAD_COMPLETE) {
+                    count += 1;
+
+                    downloadPageImage(page);
+                }
+            }
+        }
+
+        public void downloadPageInfo(PageDownload page) {
+            PageInfoDownloader downloader = new PageInfoDownloader(page);
+
+            downloader.start();
+        }
+
+        public void downloadPageImage(PageDownload page) {
+            PageImageDownloader downloader = new PageImageDownloader(page);
+
+            downloader.start();
+        }
+    }
+
+    public interface OnChapterDownloadProgress {
+        public void downloadStarted();
+        public void downloadProgress(int current, int total);
+        public void downloadComplete(boolean success);
+    }
+
+    public Scraper(DB _db, Downloader _downloader) {
+        db = _db;
+        downloader = _downloader;
+    }
+
+    public void downloadChapter(long chapterId, String targetPath,
+                                String tempDir,
+                                OnChapterDownloadProgress listener) {
+        ContentValues chapter = db.getChapter(chapterId);
+        ChapterDownload download = new ChapterDownload();
+
+        download.id = chapterId;
+        download.chapter = chapter;
+        download.listener = listener;
+        download.tempDir = tempDir;
+        download.targetPath = targetPath;
+
+        // TODO notify when it really starts
+        download.listener.downloadStarted();
+
+        if (chapter.getAsInteger(DB.DOWNLOAD_STATUS) != DB.DOWNLOAD_COMPLETE)
+            downloadPageListAndPages(download);
+        else
+            downloadPages(download);
+    }
+
+    private void downloadPages(ChapterDownload download) {
+        Cursor pageCursor = db.getPages(download.id);
+        List<PageDownload> pageList = new ArrayList<PageDownload>();
+        final int idI = pageCursor.getColumnIndex(DB.ID);
+        final int urlI = pageCursor.getColumnIndex(DB.PAGE_URL);
+        final int imageUrlI = pageCursor.getColumnIndex(DB.PAGE_IMAGE_URL);
+        final int statusI = pageCursor.getColumnIndex(DB.DOWNLOAD_STATUS);
+
+        while (pageCursor.moveToNext()) {
+            PageDownload page = new PageDownload();
+
+            page.id = pageCursor.getInt(idI);
+            page.url = pageCursor.getString(urlI);
+            page.imageUrl = pageCursor.getString(imageUrlI);
+            page.status = pageCursor.getInt(statusI);
+
+            String imgName =
+                download.chapter.getAsString(DB.CHAPTER_MANGA_ID) + "-" +
+                Long.toString(download.id) + "-" +
+                Long.toString(page.id) + ".jpg";
+
+            page.targetPath = new File(download.tempDir, imgName)
+                .getAbsolutePath();
+
+            pageList.add(page);
+        }
+
+        PageDownloader pages = new PageDownloader(download, pageList);
+
+        pages.start();
+    }
+
+    private void downloadPageListAndPages(ChapterDownload download) {
+        ChapterDownloader chapter = new ChapterDownloader(download);
+
+        chapter.start();
+    }
+
+    public static List<String> scrapeChapterPages(
+            Downloader.DownloadDestination target) {
+        Document doc;
+
+        try {
+            doc = Jsoup.parse(target.stream, target.encoding, target.baseUrl);
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Element page = doc.select("select[name=page]").first();
+
+        if (!page.hasAttr("onchange"))
+            return null;
+        Elements options = page.select("option");
+        String urlTemplate = page.attr("onchange");
+        List<String> result = new ArrayList<String>();
+
+        urlTemplate = urlTemplate.replaceFirst(".*(http:.*\\.html).*", "$1");
+
+        for (Element option : options) {
+            if (!option.hasAttr("value"))
+                continue;
+            result.add(urlTemplate.replaceFirst(
+                           "'\\s*\\+\\s*this\\.value\\s*\\+\\s*\\'",
+                           option.attr("value")));
+        }
+
+        return result;
+    }
+
+    public static String scrapeImageUrl(Downloader.DownloadDestination target) {
+        Document doc;
+
+        try {
+            doc = Jsoup.parse(target.stream, target.encoding, target.baseUrl);
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Element img = doc.select("img.chapter_img").first();
+
+        if (!img.hasAttr("src"))
+            return null;
+
+        return img.attr("src");
+    }
+
+    public static void createChapterArchive(ChapterDownload chapter,
+                                            List<PageDownload> pages) {
+        List<String> paths = new ArrayList<String>();
+
+        for (PageDownload page : pages)
+            paths.add(page.targetPath);
+
+        CBZFile.createFile(chapter.targetPath, paths);
+    }
+}
---
+++ tests/src/org/barbon/mangaget/scrape/animea/ScraperTest.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright (c) Mattia Barbon <mattia@barbon.org>
+ * distributed under the terms of the MIT license
+ */
+
+package org.barbon.mangaget.scrape;
+
+import android.content.Context;
+
+import android.os.Environment;
+
+import android.test.InstrumentationTestCase;
+
+import java.io.File;
+
+import org.barbon.mangaget.data.DB;
+
+import org.barbon.mangaget.tests.DummyDownloader;
+import org.barbon.mangaget.tests.R;
+
+import org.barbon.mangaget.scrape.animea.Scraper;
+
+public class ScraperTest extends InstrumentationTestCase {
+    private DummyDownloader downloader;
+    private DB db;
+    private Context testContext;
+    private File tempDir;
+
+    private class Progress implements Scraper.OnChapterDownloadProgress {
+        public boolean started, complete;
+
+        @Override
+        public void downloadStarted() {
+            started = true;
+        }
+
+        @Override
+        public void downloadProgress(int current, int total) {
+        }
+
+        @Override
+        public void downloadComplete(boolean success) {
+            complete = true;
+        }
+    }
+
+    @Override
+    public void setUp() throws Exception {
+        testContext = getInstrumentation().getContext()
+            .createPackageContext("org.barbon.mangaget.tests", 0);
+        downloader = new DummyDownloader(testContext);
+
+        // set up dummy pages
+        String base =
+            "http://manga.animea.net/papillon-hana-to-chou-chapter-1-page-";
+
+        downloader.addUrl(base + "1.html", R.raw.papillon_c1_p1_html);
+
+        for (int i = 2; i < 46; ++i)
+            downloader.addUrl(base + Integer.toString(i) + ".html",
+                              R.raw.papillon_c1_p2_html);
+
+        // set up dummy images
+        String p1 = "http://s2-a.animea-server.net/5338%2F1_JHMCN%2F00_fuuchifighters.jpg" ;
+        String pn = "http://s2-a.animea-server.net/5338%2F1_JHMCN%2F001_cover.jpg" ;
+
+        downloader.addUrl(p1, R.raw.papillon_dummy_img);
+        downloader.addUrl(pn, R.raw.papillon_dummy_img);
+
+        // set up dummy database
+        db = DB.getNewInstance(getInstrumentation().getTargetContext(),
+                               "manga_test");
+
+        // download directory
+        File externalStorage = Environment.getExternalStorageDirectory();
+        tempDir = new File(externalStorage, "MangaGetTest");
+        tempDir.mkdir();
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+    }
+
+    public void testFullDownload() throws Throwable {
+        final long mangaId = db.insertManga(
+            "Title", "MangaGetTest/Dummy-%chapter%.cbz",
+            "http://manga.animea.net/papillon-hana-to-chou.html");
+        final long chapterId = db.insertChapter(
+            mangaId, 1, 45, "Chapter title",
+            "http://manga.animea.net/papillon-hana-to-chou-chapter-1-page-1.html");
+        final String targetCbz = new File(tempDir, "Dummy-1.cbz")
+            .getAbsolutePath();
+        final String tempDirString = tempDir.getAbsolutePath();
+        final Scraper scraper = new Scraper(db, downloader);
+        final Progress progress = new Progress();
+
+        class UiTask implements Runnable {
+            @Override
+            public void run() {
+                scraper.downloadChapter(
+                    chapterId, targetCbz, tempDirString, progress);
+            }
+        }
+
+        UiTask uiTask = new UiTask();
+
+        runTestOnUiThread(uiTask);
+
+        while (!progress.complete)
+            Thread.sleep(500);
+
+        System.out.println("Temp dir: " + tempDirString);
+        System.out.println("Target: " + targetCbz);
+
+        assertTrue(progress.started);
+        assertTrue(progress.complete);
+        assertTrue(new File(targetCbz).exists());
+    }
+}
