c495d1629214f1f150ac21411462160b8c6518d6
Mattia Barbon <mattia@barbon.org>
2011-08-26 17:52:23 +0200

    Added context menu on long manga click (for now, only has an option to refresh chapter list).

--- src/org/barbon/mangaget/fragments/MangaList.java
+++ src/org/barbon/mangaget/fragments/MangaList.java
@@ -7,15 +7,20 @@ package org.barbon.mangaget.fragments;
 
 import android.os.Bundle;
 
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
 import android.support.v4.app.ListFragment;
 
+import org.barbon.mangaget.Download;
 import org.barbon.mangaget.R;
 
 import org.barbon.mangaget.data.DB;
@@ -39,6 +44,8 @@ public class MangaList extends ListFragment {
             new String[] { DB.MANGA_TITLE },
             new int[] { R.id.item_text });
         setListAdapter(adapter);
+
+        registerForContextMenu(getListView());
     }
 
     @Override
@@ -70,5 +77,28 @@ public class MangaList extends ListFragment {
     public void onListItemClick (ListView l, View v, int position, long id) {
         if (onMangaSelected != null)
             onMangaSelected.onMangaSelected(id);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getActivity().getMenuInflater();
+
+        inflater.inflate(R.menu.manga_context, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info =
+            (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+
+        switch (item.getItemId()) {
+        case R.id.refresh:
+            Download.startMangaUpdate(getActivity(), info.id);
+            return true;
+        default:
+            return super.onContextItemSelected(item);
+        }
     }
 }
