c5f63c5f1aa7eb735c205440b4295face349e08f
Mattia Barbon <mattia@barbon.org>
2011-09-27 22:13:41 +0200

    Added generic interface for activities that can be cancelled in mid execution.

---
+++ src/org/barbon/mangaget/PendingTask.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) Mattia Barbon <mattia@barbon.org>
+ * distributed under the terms of the MIT license
+ */
+
+package org.barbon.mangaget;
+
+public interface PendingTask {
+    public void cancel();
+}
