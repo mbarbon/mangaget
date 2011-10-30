b86b5f5f43703b1f1c6b8918cf88c3e724d494bb
Mattia Barbon <mattia@barbon.org>
2011-10-30 17:29:07 +0100

    Keep all the fragments in a single activity.

--- src/org/barbon/mangaget/Main.java
+++ src/org/barbon/mangaget/Main.java
@@ -14,47 +14,66 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 
 import org.barbon.mangaget.fragments.ChapterList;
 import org.barbon.mangaget.fragments.MangaList;
 
 public class Main extends FragmentActivity {
+    private static final String MANGA_LIST = "manga_list";
+    private static final String CHAPTER_LIST = "chapter_list";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.main);
 
-        final MangaList mangaList = (MangaList)
-            getSupportFragmentManager().findFragmentById(R.id.manga_list);
-        final ChapterList chapterList = (ChapterList)
-            getSupportFragmentManager().findFragmentById(R.id.chapter_list);
+        // needs to be flushed here otherwise the functions belog
+        // sees the wrong state
+        clearBackStack();
 
-        if (   mangaList != null && chapterList != null
-            && chapterList.isInLayout()) {
+        boolean isLandscape = findViewById(R.id.landscape_container) != null;
+
+        if (isLandscape) {
             // landscape
+            FragmentTransaction transaction = beginTransaction(false);
+            MangaList mangaList = setMangaList(transaction, true);
+
+            setChapterList(transaction, true);
+            transaction.commit();
+
             mangaList.setOnMangaSelected(
                 new MangaList.OnMangaSelected() {
                     public void onMangaSelected(long mangaId) {
-                        chapterList.loadChapterList(mangaId);
+                        getChapterList().loadChapterList(mangaId);
                     }
                 });
         }
         else {
             // portrait
+            FragmentTransaction transaction = beginTransaction(false);
+            MangaList mangaList = setMangaList(transaction, true);
+
+            setChapterList(transaction, false);
+            transaction.commit();
+
             mangaList.setOnMangaSelected(
                 new MangaList.OnMangaSelected() {
-                    private Intent chapters =
-                        new Intent(Main.this, Chapters.class);
-
                     public void onMangaSelected(long mangaId) {
-                        chapters.putExtra(Chapters.MANGA_ID, mangaId);
+                        ChapterList chapterList = pushChapterList();
 
-                        startActivity(chapters);
+                        chapterList.loadChapterList(mangaId);
                     }
                 });
         }
+
+        // needs to be flushed here otherwise the onMangaSelected
+        // callback does not see the changes above
+        getSupportFragmentManager().executePendingTransactions();
 
         // start listening for network connectivity changes and resume
         // pending downloads if any
@@ -83,5 +102,89 @@ public class Main extends FragmentActivity {
         default:
             return super.onOptionsItemSelected(item);
         }
+    }
+
+    // implementation
+
+    private ChapterList pushChapterList() {
+        FragmentTransaction transaction = beginTransaction(true);
+        ChapterList chapterList = setChapterList(transaction, true);
+
+        setMangaList(transaction, false);
+        transaction.commit();
+
+        return chapterList;
+    }
+
+    private void clearBackStack() {
+        FragmentManager manager = getSupportFragmentManager();
+
+        // clear the backstack
+        for (int i = manager.getBackStackEntryCount(); i != 0; --i)
+            manager.popBackStack();
+
+        manager.executePendingTransactions();
+    }
+
+    private FragmentTransaction beginTransaction(boolean push) {
+        FragmentTransaction transaction =
+            getSupportFragmentManager().beginTransaction();
+
+        transaction.setTransition(FragmentTransaction.TRANSIT_NONE);
+
+        if (push)
+            transaction.addToBackStack(null);
+
+        return transaction;
+    }
+
+    private <F extends Fragment> F setFragment(Class<F> klass,
+            FragmentTransaction transaction, int id,
+            String tag, boolean status) {
+        F fragment = klass.cast(getSupportFragmentManager()
+                                .findFragmentByTag(tag));
+
+        if ((fragment != null && fragment.isAdded()) == status)
+            return fragment;
+
+        if (status) {
+            if (fragment == null) {
+                try {
+                    fragment = klass.newInstance();
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException(e);
+                } catch (InstantiationException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            transaction.add(id, fragment, tag);
+        }
+        else
+            transaction.remove(fragment);
+
+        return fragment;
+    }
+
+    private MangaList setMangaList(
+            FragmentTransaction transaction, boolean status) {
+        return setFragment(MangaList.class, transaction,
+                           R.id.manga_list, MANGA_LIST, status);
+    }
+
+    private ChapterList setChapterList(
+            FragmentTransaction transaction, boolean status) {
+        return setFragment(ChapterList.class, transaction,
+                           R.id.chapter_list, CHAPTER_LIST, status);
+    }
+
+    private MangaList getMangaList() {
+        return (MangaList) getSupportFragmentManager()
+            .findFragmentByTag(MANGA_LIST);
+    }
+
+    private ChapterList getChapterList() {
+        return (ChapterList) getSupportFragmentManager()
+            .findFragmentByTag(CHAPTER_LIST);
     }
 }
